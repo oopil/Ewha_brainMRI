d620a721768312d32bcff1fd1fc7f688026188a5
o_opil <xim1110@naver.com>
2019-05-22 14:48:22 +0900

    default

--- fractal_dimension.py
+++ fractal_dimension.py
@@ -1,11 +1,28 @@
 import os
-import matlab.engine
-
+import matlab.engine as Matlab
 
 file_name = "/home/soopil/Desktop/Dataset/brain_ewha/Meningioma_only_T1C_masks/1550930_CE-label.nrrd"
 dir_path =  "/home/soopil/Desktop/Dataset/brain_ewha/Meningioma_only_T1C_masks/"
-
+fd_result_file = "./fd_result_file"
 file_list = os.listdir(dir_path)
-print(file_list)
-eng = matlab.engine.start_matlab()
-tf = eng.boxcount()
+print(len(file_list))
+
+eng = Matlab.start_matlab()
+fd = open(fd_result_file, 'a+t')
+for name in file_list:
+    split = name.split('_')
+    if split[0] == 'norm':
+        subj_name = split[1]
+    else:
+        subj_name = split[0]
+    # print(name, subj_name)
+    print(subj_name)
+    file_path = os.path.join(dir_path, name)
+    array = eng.nrrdread(file_path)
+    result= eng.boxcount(array)
+    # print(','.join(str(e) for e in [12, 3, 4, 5]))
+    print(subj_name, result)
+    line = subj_name +'/'+','.join(str(e) for e in result)
+    fd.write(line+'\n')
+
+fd.close()
